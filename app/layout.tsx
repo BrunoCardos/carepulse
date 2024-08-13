5fa28a1944af3599f3d742665114b3f9bc953852
Bruno Cardoso <brunocardoso@sapo.cv>
2024-08-13 15:36:26 -0100

    feat: complete the main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased text-white', fontSans.variable)}>
         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
